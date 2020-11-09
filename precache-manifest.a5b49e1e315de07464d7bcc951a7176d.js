df3e76845cfa03932490a9c9c98d01b129ef5380
Daniel-99 <daniel.leguia99@gmail.com>
2020-11-09 17:07:14 -0500

    Updates

---
+++ precache-manifest.a5b49e1e315de07464d7bcc951a7176d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66f4757be4ab3830cf3c07a2caf70f5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ab922f99f8601b18cdaa",
+    "url": "/my-app/static/css/main.ed50d581.chunk.css"
+  },
+  {
+    "revision": "dd02c542874fa2a75133",
+    "url": "/my-app/static/js/2.9564f95b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/my-app/static/js/2.9564f95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab922f99f8601b18cdaa",
+    "url": "/my-app/static/js/main.a230a218.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5f5108a7dd998d4d8542afec688bf5c6",
+    "url": "/my-app/static/media/logo.5f5108a7.png"
+  }
+]);
